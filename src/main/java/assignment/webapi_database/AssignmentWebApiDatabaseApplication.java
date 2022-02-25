b713db7586d6285640077c0caff89a34b4d981fb
savannah-borst <savannah.borst@gmail.com>
2022-02-25 15:38:21 +0100

    Initializing project with dependencies

---
+++ src/main/java/assignment/webapi_database/AssignmentWebApiDatabaseApplication.java
@@ -0,0 +1,13 @@
+package assignment.webapi_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentWebApiDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentWebApiDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/assignment/webapi_database/AssignmentWebApiDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package assignment.webapi_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentWebApiDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
