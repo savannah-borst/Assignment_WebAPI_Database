79de779e0fae4abe1ed603eecf3d568a83f6d1e2
savannah-borst <savannah.borst@gmail.com>
2022-02-27 14:12:06 +0100

    corrected typo

--- src/main/java/assignment/webapi_database/Repositories/MovieRepository.java
+++ src/main/java/assignment/webapi_database/Repositories/MovieRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MovieRespository extends JpaRepository<Movie, Integer> {
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
 }
