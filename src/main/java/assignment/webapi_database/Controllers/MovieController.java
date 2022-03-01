4996227f249160a6232de4554f6c981f06cf5036
savannah-borst <savannah.borst@gmail.com>
2022-03-01 12:25:47 +0100

    Create and get all movie controllers

--- src/main/java/assignment/webapi_database/Controllers/MovieController.java
+++ src/main/java/assignment/webapi_database/Controllers/MovieController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class MovieController {
@@ -16,9 +18,16 @@ public class MovieController {
 
     //create
     @PostMapping("/movie")
-    public Movie createMovie(@RequestBody Movie movie) {
-        movie = movieRepository.save(movie);
-        return movie;
+    public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
+        HttpStatus status;
+
+        if (movieRepository.existsById(movie.movieId)) {
+            status = HttpStatus.BAD_REQUEST;
+        }  else {
+            movie = movieRepository.save(movie);
+            status = HttpStatus.OK;
+        }
+        return new ResponseEntity<>(movie, status);
     }
 
     //read
@@ -37,10 +46,16 @@ public class MovieController {
         return new ResponseEntity<>(movie, status);
     }
 
+    //get all
+    @GetMapping("movie/all")
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        List<Movie> movies = movieRepository.findAll();
+        HttpStatus status = HttpStatus.OK;
+        return new ResponseEntity<>(movies, status);
+    }
 
     //update
-
     //delete
 
-    //get all
+
 }
