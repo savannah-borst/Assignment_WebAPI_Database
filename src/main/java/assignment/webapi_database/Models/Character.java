438638452b3d90c9c3a5cb2531d2bb7ecc6864b2
Timothy Blom <kracok@gmail.com>
2022-03-01 12:37:17 +0100

    no changes

--- src/main/java/assignment/webapi_database/Models/Character.java
+++ src/main/java/assignment/webapi_database/Models/Character.java
@@ -1,5 +1,7 @@
 package assignment.webapi_database.Models;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+
 import javax.persistence.*;
 
 @Entity
@@ -20,4 +22,25 @@ public class Character {
 
     @Column
     public String picture;
+
+//    @JsonGetter("owner")
+//    public String getOwner()
+//    {
+//        if(owner != null)
+//        {
+//            return "/person/" + owner.id;
+//        }
+//        else
+//        {
+//            return null;
+//        }
+//    }
+//
+//    @ManyToOne()
+//    @JoinTable(
+//            name = "owner",
+//            joinColumns = {@JoinColumn(name = "pet_id")},
+//            inverseJoinColumns = {@JoinColumn(name = "owner_id")}
+//    )
+
 }
--- src/main/java/assignment/webapi_database/Models/Movie.java
+++ src/main/java/assignment/webapi_database/Models/Movie.java
@@ -1,5 +1,7 @@
 package assignment.webapi_database.Models;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+
 import javax.persistence.*;
 import java.util.Set;
 
@@ -10,8 +12,6 @@ public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public int movieId;
-
-//    private Set<Character> movieCharacters = new HashSet<>(); this is a test merge
 
     @Column(length = 60)
     public String title;
@@ -30,4 +30,16 @@ public class Movie {
 
     @Column
     public String trailer;
+
+//    @JsonGetter("pets")
+//    public List<String> get_pets_list(){
+//        return pets.stream()
+//                .map(pet -> {
+//                    return "/pet/" +pet.id;
+//                }).collect(Collectors.toList());
+//    }
+//
+//    @OneToMany(mappedBy = "owner", fetch = FetchType.LAZY)
+//    List<Pet> pets = new ArrayList<>();
+
 }
