a449e7bae32f1be231775dda3244567a5d6380bc
savannah-borst <savannah.borst@gmail.com>
2022-03-03 14:09:09 +0100

    made franchiseService

---
+++ src/main/java/assignment/webapi_database/Service/FranchiseService.java
@@ -0,0 +1,44 @@
+package assignment.webapi_database.Service;
+
+import assignment.webapi_database.Models.Franchise;
+import assignment.webapi_database.Models.Movie;
+import assignment.webapi_database.Repositories.FranchiseRepository;
+import assignment.webapi_database.Repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FranchiseService {
+    @Autowired
+    private FranchiseRepository franchiseRepository;
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> updateCharInMovie(Integer franchiseId, Integer[] movieId)  {
+        Optional<Franchise> repoFran = franchiseRepository.findById(franchiseId);
+        Franchise franchise = repoFran.orElseThrow();
+        List<Movie> movies = franchise.getMovies();
+        boolean contains;
+
+        for (int id: movieId) {
+            contains = movies.stream().anyMatch(movie -> movie.movieId == id);
+            if (!contains) {
+                if (movieRepository.existsById(id)) {
+                    Optional<Movie> movieItem = movieRepository.findById(id);
+                    movies.add(movieItem.orElse(null));
+                }
+            }
+        }
+
+        franchise.setMovies(movies);
+        franchiseRepository.save(franchise);
+
+        return franchise.getMovies();
+
+    }
+}
+
