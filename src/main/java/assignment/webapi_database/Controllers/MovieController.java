5254c6fd4763b763d3074dd7a880c5d17ada477f
savannah-borst <savannah.borst@gmail.com>
2022-03-03 14:08:08 +0100

    delete autwired charrepo

--- src/main/java/assignment/webapi_database/Controllers/MovieController.java
+++ src/main/java/assignment/webapi_database/Controllers/MovieController.java
@@ -21,8 +21,6 @@ public class MovieController {
     @Autowired
     private MovieRepository movieRepository;
     @Autowired
-    private CharacterRepository characterRepository;
-    @Autowired
     private MovieService movieService;
 
     //create
