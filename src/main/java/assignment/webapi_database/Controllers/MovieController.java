bc7a8d56ea8bfd57ff058e494603d6b3bf03ceee
savannah-borst <savannah.borst@gmail.com>
2022-03-03 09:39:10 +0100

    changing update movie and create movie. + adding update char in movie

--- src/main/java/assignment/webapi_database/Controllers/MovieController.java
+++ src/main/java/assignment/webapi_database/Controllers/MovieController.java
@@ -1,6 +1,8 @@
 package assignment.webapi_database.Controllers;
 
+import assignment.webapi_database.Models.Character;
 import assignment.webapi_database.Models.Movie;
+import assignment.webapi_database.Repositories.CharacterRepository;
 import assignment.webapi_database.Repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,18 +18,17 @@ public class MovieController {
 
     @Autowired
     private MovieRepository movieRepository;
+    @Autowired
+    private CharacterRepository characterRepository;
 
     //create
     @PostMapping("/create")
     public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
         HttpStatus status;
 
-        if (movieRepository.existsById(movie.movieId)) {
-            status = HttpStatus.BAD_REQUEST;
-        }  else {
-            movie = movieRepository.save(movie);
-            status = HttpStatus.OK;
-        }
+        movie = movieRepository.save(movie);
+        status = HttpStatus.OK;
+
         return new ResponseEntity<>(movie, status);
     }
 
@@ -55,52 +56,62 @@ public class MovieController {
         return new ResponseEntity<>(movies, status);
     }
 
+    //Get Characters in movie
+    @GetMapping("/{id}/characters")
+    public ResponseEntity<List<String>> getCharacterInMovie(@PathVariable Integer id) {
+        Movie movie  = movieRepository.findById(id).get();
+        List<String> listCharacters = movie.get_character_list();
+        HttpStatus status = HttpStatus.OK;
+
+        return new ResponseEntity<>(listCharacters, status);
+    }
+
     //update
     @PutMapping("/update/{id}")
     public ResponseEntity<Movie> updateMovie(@PathVariable Integer id, @RequestBody Movie movie) {
-        Movie returnMovie = new Movie();
+        Movie returnMovie = movieRepository.findById(id).get();
         HttpStatus status;
 
-        if (movieRepository.existsById(movie.movieId)) {
-            Optional<Movie> repoMovie = movieRepository.findById(id);
-            returnMovie = repoMovie.get();
-
-            //if (movie.movieId == returnMovie.movieId) {
-                //needed?
-            //}
-
-            if (movie.title != null) {
-                returnMovie.title = movie.title;
-            }
-
-            if (movie.genre != null) {
-                returnMovie.genre = movie.genre;
-            }
-
-            if (movie.releaseYear != null) {
-                returnMovie.releaseYear = movie.releaseYear;
-            }
-
-            if (movie.director != null) {
-                returnMovie.director = movie.director;
-            }
-
-            if (movie.picture != null) {
-                returnMovie.picture = movie.picture;
-            }
-
-            if (movie.trailer != null) {
-                returnMovie.trailer = movie.trailer;
-            }
-
-            returnMovie = movieRepository.save(movie);
-            status = HttpStatus.NO_CONTENT;
-
-            return new ResponseEntity<>(returnMovie, status);
-        } else {
+        if(!id.equals(movie.getMovieId())) {
             status = HttpStatus.BAD_REQUEST;
             return new ResponseEntity<>(returnMovie, status);
         }
+
+        if(movie.getTitle() != null) {
+            returnMovie.title = movie.title;
+        }
+
+        if(movie.getGenre() != null) {
+            returnMovie.genre = movie.genre;
+        }
+
+        if(movie.getReleaseYear() != null) {
+            returnMovie.releaseYear = movie.releaseYear;
+        }
+
+        if(movie.getDirector() != null) {
+            returnMovie.director = movie.director;
+        }
+
+        if(movie.getPicture() != null) {
+            returnMovie.picture = movie.picture;
+        }
+
+        if(movie.getTrailer() != null) {
+            returnMovie.trailer = movie.trailer;
+        }
+
+        if(movie.getCharacters() != null) {
+            returnMovie.characters = movie.characters;
+        }
+
+        if(movie.getFranchise() != null) {
+            returnMovie.franchise = movie.franchise;
+        }
+
+        returnMovie = movieRepository.save(returnMovie);
+        status = HttpStatus.NO_CONTENT;
+        return new ResponseEntity<>(returnMovie, status);
     }
 
     //delete
@@ -114,7 +125,42 @@ public class MovieController {
             movieRepository.deleteById(id);
             status = HttpStatus.OK;
         }
-
         return new ResponseEntity<>(status);
     }
+
+    //update characters in movies
+    //Credits to Iljaas and Richie for stream api https://github.com/iljaasdhonre/Assignment_3_WebApiAndDatabase/blob/master/src/main/java/com/richieandmod/assignment_3_webapianddatabase/Services/MovieServiceImpl.java
+    @PutMapping("/{id}/update/character")
+    public ResponseEntity<Movie> updateCharacterInMovie(@PathVariable Integer id, @RequestBody int[] charId)  {
+        Movie movie = movieRepository.getById(id);
+        List<Character> actors = movie.getCharacters();
+        boolean contains;
+        HttpStatus status;
+
+        if (movieRepository.existsById(id)) {
+
+            for (int i: charId) {
+                contains = actors.stream().anyMatch(character -> character.getCharacterId() == i);
+                if (!contains) {
+                    if (characterRepository.existsById(i)) {
+                        Optional<Character> character = characterRepository.findById(i);
+                        actors.add(character.orElse(null));
+                    }
+                }
+            }
+
+            movie.setCharacters(actors);
+            movieRepository.save(movie);
+
+            status = HttpStatus.OK;
+
+            return new ResponseEntity<>(movie, status);
+
+        } else {
+            status = HttpStatus.BAD_REQUEST;
+            return new ResponseEntity<>(movie, status);
+        }
+    }
+
 }
+
